152e80d2e27f8a17f0da1f17b24967242c904d3c
Damian Aguado <damianaguado996@gmail.com>
2024-05-09 17:35:31 -0300

    Probando para el futuro

---
+++ README.md
@@ -0,0 +1,4 @@
+1. Add Header
+    1. Generate Component
+    2. Add Html
+    3. Add css
---
+++ backend/src/server.ts
@@ -0,0 +1,34 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express from "express";
+import cors from "cors";
+import foodRouter from './routers/food.router';
+import userRouter from './routers/user.router';
+import orderRouter from './routers/order.router';
+import { dbConnect } from './configs/database.config';
+
+dbConnect();
+
+const app = express();
+app.use(cors({
+    credentials:true,
+    origin:["http://localhost:4200"]
+}));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");    
+    next();
+  });
+  
+app.use(express.json());
+
+
+app.use("/api/foods", foodRouter);
+app.use("/api/users", userRouter);
+app.use("/api/orders", orderRouter);
+
+const port = 5000;
+app.listen(port, () => {
+    console.log("Website served on http://localhost:"+port);
+});
